ef77ff1d40835c348eb7412ee5aec2b9414249b3
Viachaslau Rovsky <vrovsky@gmail.com>
2021-08-01 22:49:17 +0300

    Conditional Statemets and Data Types completed

---
+++ Conditional Statements and Data Types/StatementsAndTypes.js
@@ -0,0 +1,19 @@
+let string = "27";
+let booleanTrue = true;
+let number = 7;
+
+console.log(string + booleanTrue);
+console.log(string + number);
+console.log(number + booleanTrue);
+
+console.log(string * booleanTrue);
+console.log(string * number);
+console.log(number * booleanTrue);
+
+console.log(string / booleanTrue);
+console.log(string / number);
+console.log(number / booleanTrue);
+
+Number(string);
+Boolean(number);
+String(booleanTrue);
