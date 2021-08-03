109eddabeefb0181dcdaf76b30b7ecd49998ea3d
Viachaslau Rovsky <vrovsky@gmail.com>
2021-08-03 22:04:26 +0300

    Regular expression task complete

---
+++ String, RegExp, DataTime/RegularExpressions.js
@@ -0,0 +1,6 @@
+let firstString = "ahb acb aeb aeeb adcb axeb";
+let firstResult = firstString.match(/a.b/g);
+alert(firstResult);
+let secondString = "2+3 223 2223";
+let secondResult = secondString.match(/2\+3/);
+alert(secondResult);
