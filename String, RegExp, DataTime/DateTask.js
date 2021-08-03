3a81b22246a057e03dc5dea20035d00f8a87ecad
Viachaslau Rovsky <vrovsky@gmail.com>
2021-08-03 22:17:25 +0300

    String, RegExp, DataTime tasks completed

---
+++ String, RegExp, DataTime/DateTask.js
@@ -0,0 +1,7 @@
+let now = new Date();
+let today = now.getDate();
+let thisMonth = now.getMonth() + 1;
+let thisYear = now.getFullYear();
+console.log(today);
+console.log(thisMonth);
+console.log(thisYear);
